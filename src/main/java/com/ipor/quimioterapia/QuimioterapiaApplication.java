abeebad9ff368baa86b28d1ef8a5c6c405ef8407
Alexander Saldaña <alexdsa981@gmail.com>
2025-05-22 14:14:40 -0500

    Crea repositorio/avance bd quimio

---
+++ src/main/java/com/ipor/quimioterapia/QuimioterapiaApplication.java
@@ -0,0 +1,13 @@
+package com.ipor.quimioterapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuimioterapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuimioterapiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipor/quimioterapia/QuimioterapiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipor.quimioterapia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuimioterapiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
